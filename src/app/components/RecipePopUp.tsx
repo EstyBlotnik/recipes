418bee6d64adde8aba77764b0fae12e05c29153d
RivkyTR <Rivka4555@gmail.com>
2024-11-13 15:17:05 +0200

    add scroll to popup & prevent scroll fot body

--- src/app/components/RecipePopUp.tsx
+++ src/app/components/RecipePopUp.tsx
@@ -10,53 +10,50 @@ interface PopUpProps {
   onClose: () => void; // Callback to close the popup
 }
 
-// useUser from import { useUser } from '@/app/hooks/useRecipeContects';
-// add star
-
 const RecipePopUp: React.FC<PopUpProps> = ({ recipe, onClose }) => {
-  
-
   useEffect(() => {
-    document.body.classList.add("no-scroll"); // מונע גלילה של הדף בעת פתיחת הפופאפ
+    document.body.classList.add("no-scroll");
+
     return () => {
-      document.body.classList.remove("no-scroll"); // מחזיר את המצב הרגיל בעת סגירה
+      document.body.classList.remove("no-scroll");
     };
   }, []);
-  
+
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-end items-start z-50 " onClick={onClose}>
+    <div
+      className="fixed inset-0 bg-black bg-opacity-50 flex justify-end items-start z-50"
+      onClick={onClose}
+    >
       {/* Popup Content */}
-      <div className="relative flex flex-row justify-center w-[350px] min-h-full bg-white overflow-y-auto shadow-xl p-6">
+      <div
+        className="relative flex flex-col w-[350px] h-full bg-white shadow-xl p-6 overflow-y-auto">
         {/* Close Button */}
-        <div>
-          <button
-            onClick={onClose}
-            className="absolute top-2 left-4 text-gray-500 hover:text-gray-800 text-4xl"
-          >
-            &times;
-          </button>
-        </div>
+        <button
+          onClick={onClose}
+          className="absolute top-2 left-4 text-gray-500 hover:text-gray-800 text-4xl"
+        >
+          &times;
+        </button>
 
         {/* Recipe Content */}
         <div className="bg-red-300 p-4">
-        <div className="flex flex-col justify-center items-center pt-12 w-[100%] ">
-          <div className=" flex flex-col items-center">
-            <h3 className="text-xl font-bold  text-gray-900 mb-2">
+          <div className="flex flex-col justify-center items-center pt-12 w-full">
+            <h3 className="text-xl font-bold text-gray-900 mb-2">
               {recipe.name}
             </h3>
 
-            {/* Category and Favorite Icon */}
             <div className="flex items-center mb-2">
-              <span className="text-sm text-gray-700 font-serif">{recipe.category}</span>
+              <span className="text-sm text-gray-700 font-serif">
+                {recipe.category}
+              </span>
               <Star />
             </div>
+
+            <div className="w-[90%] justify-center mb-4">
+              <Image imageUrl={recipe.img} width={400} />
+            </div>
           </div>
-          <div className="w-[90%] justify-center mb-4">
-            {/* <Image src={recipe.img} alt={'recipe picture'} width={300} height={200} /> */}
-            <Image imageUrl={recipe.img} width={400} />
-          </div>
-          </div>
-          {/* Ingredients */}
+
           <div className="mb-4">
             <span className="text-base font-semibold text-gray-800 mb-2">
               Ingredients
@@ -68,14 +65,12 @@ const RecipePopUp: React.FC<PopUpProps> = ({ recipe, onClose }) => {
             </ul>
           </div>
 
-            {/* Instructions */}
-            <div>
-              <h3 className="text-base font-semibold text-gray-800 mb-2">
-                Instructions
-              </h3>
-              <p className="text-gray-700 text-xs">{recipe.instructions}</p>
-            </div>
-
+          <div>
+            <h3 className="text-base font-semibold text-gray-800 mb-2">
+              Instructions
+            </h3>
+            <p className="text-gray-700 text-xs">{recipe.instructions}</p>
+          </div>
         </div>
       </div>
     </div>
