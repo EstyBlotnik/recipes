2e3778c3ac5628f5d6d988d550063c4cb7d9cb50
SariHop <sarihop12@gmail.com>
2024-11-14 15:09:05 +0200

    vercel error

--- src/app/components/Star.tsx
+++ src/app/components/Star.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { RecipeTypeWithId } from '../types/irecipe';
 import { useRecipeContecst } from '@/app/hooks/useRecipeContects';
 import { updateFavorite } from '@/app/services/recipeCrud';
