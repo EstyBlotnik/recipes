d67bc788ac88618177d45dea3a9dc887200a321d
tamar <tamarlouf1@gmail.com>
2024-11-14 11:34:56 +0200

    star without render

--- src/app/services/recipeCrud.ts
+++ src/app/services/recipeCrud.ts
@@ -10,7 +10,8 @@ export const fetchAllRecipes = async (): Promise<RecipeTypeWithId[]> => {
   } catch (error: unknown) {
     if (axios.isAxiosError(error)) {
       throw new Error(
-        `Error fetching recipes: ${error.response?.data?.message || error.message
+        `Error fetching recipes: ${
+          error.response?.data?.message || error.message
         }`
       );
     } else {
@@ -24,25 +25,14 @@ export const getCategories = async () => {
     const response = await axios.get(`${apiUrl}categories`);
     return response.data;
   } catch (error) {
-    console.error('Error getting categories:', error);
-    throw error;
-  }
-}
-
-
-export const addarecipe = async (formData: RecipeType) => {
-  try {
-    const response = await axios.post(apiUrl, formData);
-    return response.data;
-  } catch (error) {
-    console.error('Error adding recipe:', error);
+    console.error("Error getting categories:", error);
     throw error;
   }
 };
 
-export const addBook = async (formData: RecipeTypeWithId) => {
+export const addarecipe = async (formData: RecipeType) => {
   try {
-    const response = await axios.post(`${apiUrl}/recipePost`, formData);
+    const response = await axios.post(apiUrl, formData);
     return response.data;
   } catch (error) {
     console.error("Error adding recipe:", error);
@@ -50,3 +40,13 @@ export const addBook = async (formData: RecipeTypeWithId) => {
   }
 };
 
+export const updateFavorite = async (id: string, favorite: boolean) => {
+  try {
+    const response = await axios.put(`${apiUrl}favorite/${id}`,{ favorite });
+    console.log("Recipe updated successfully:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error update favorite recipe:", error);
+    throw error;
+  }
+};
