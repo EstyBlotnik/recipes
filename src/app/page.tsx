7bfc9beb5f3f34c93e552f8bc626e3c8615ac626
RivkyTR <Rivka4555@gmail.com>
2024-11-17 11:03:55 +0200

    login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import ConnectingModal from './components/ConnectingModal'
+import '../app/globals.css'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+      <div className='bg-sky-600 fixed top-0 w-full h-[13vh] flex flex-row justify-between font-bold items-center text-white p-3 z-10'>
+        <div className='font-serif text-base'>
+          My Recipes
+        </div>
+      </div>
+    <div className="paging">
+    <ConnectingModal/>
+    </div>
+    </div>
   )
 }
 
