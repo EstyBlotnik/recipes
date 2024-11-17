a9c0cd6483a336bfba50ec1f6ffd4b37f76e35f3
EstyBlotnik <118099586+EstyBlotnik@users.noreply.github.com>
2024-11-17 14:18:46 +0200

    create conflikts

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -2,9 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import jwt from 'jsonwebtoken';
 
 export async function GET(req: NextRequest) {
- 
   const token = req.cookies.get("token"); // בגרסה זו יש גישה ישירה לעוגיות דרך req.cookies
-
   // אם יש token, מחזירים תשובה עם מידע מאובטח
   if (token) {
     return NextResponse.json({ message: "Protected data", token });
@@ -16,23 +14,20 @@ export async function GET(req: NextRequest) {
 
 export async function POST(req: NextRequest) {
   const { email } = await req.json();
-
   if (email === "user@example.com") {
     // יצירת token דינמי עם jwt
-
-
     const token = jwt.sign(
       { email }, // payload - אתה יכול לשים כל מידע שצריך כאן
       process.env.JWT_SECRET!, // סוד ליצירת ה-token
       { expiresIn: '1m' } // הגדרת תוקף
     );
-
+    
     const headers = new Headers();
     headers.append(
       "Set-Cookie",
       `token=${token}; path=/; secure; HttpOnly; SameSite=Strict`
     );
-
+    
     // החזרת תשובה עם ה-token בעוגיה
     return NextResponse.json(
       { message: "Login successful", token },
